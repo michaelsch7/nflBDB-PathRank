dd9843f6367b69e61784b51b5714aa7f58cc789e
Michael Schwartz <mschw19@lsu.edu>
2022-12-10 00:58:51 -0600

    area under curve ref

--- src/PathRank.py
+++ src/PathRank.py
@@ -9,6 +9,8 @@ PLAYERS = pd.read_csv("data/players.csv")
 SCOUTING = pd.read_csv("data/pffScoutingData.csv")
 PLAYID = 97  
   
+  
+# area under curve https://www.includehelp.com/python/hatching-the-area-between-two-curves.aspx
 def main():
     # Run drawPath - Expected output picture    
     drawPath(WEEKDF, PLAYS, PLAYERS, SCOUTING, PLAYID)
